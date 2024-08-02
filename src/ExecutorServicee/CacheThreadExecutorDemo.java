9ee3c317f854d81d4bf7f6dd8e2bdf3e3baf091a
beingyash <beingyash2712@gmail.com>
2024-08-02 12:00:04 +0530

    cache thread executor

---
+++ src/ExecutorServicee/CacheThreadExecutorDemo.java
@@ -0,0 +1,29 @@
+package ExecutorServicee;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+public class CacheThreadExecutorDemo {
+    public static void main(String[] args) {
+
+        // created around 16 threads
+        ExecutorService service =Executors.newCachedThreadPool();
+
+        for(int i=0;i<50;i++){
+            service.execute(new Task(i));
+        }
+    }
+}
+
+class Test2  implements  Runnable{
+    private int  index ;
+
+    Test2(int i ){
+        this.index=i;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread " + Thread.currentThread().getName() + "Running Task" + this.index);
+    }
+}
